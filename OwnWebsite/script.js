cd259082b5ba3146c17cb64da0cfe376b49cd629
marc.prem09@gmail.com <marc.prem09@gmail.com>
2023-10-08 19:14:26 +0200

    test

---
+++ OwnWebsite/script.js
@@ -0,0 +1,11 @@
+let mediaQueryObj = window.matchMedia('(prefers-color-scheme: dark)');
+let isDarkMode = mediaQueryObj.matches;
+
+if (navigator.userAgent.match(/samsung/i)) {
+  if (isDarkMode) {
+    alert("Your browser (Samsung Internet) may not show this website" + 
+    "correctly. pLease consider using a standards-compliant" + 
+    "browser instead. \n\n" + 
+    "We recommend FireFox, Microsoft Edge, or Google Chrome.");
+  }
+}
