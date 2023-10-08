400197f33b5852f1c8417371a021108f38ccb5ec
marc.prem09@gmail.com <marc.prem09@gmail.com>
2023-10-08 19:42:50 +0200

    test

--- OwnWebsite/script.js
+++ OwnWebsite/script.js
@@ -1,11 +1,11 @@
-let mediaQueryObj = window.matchMedia('(prefers-color-scheme: dark)');
+/*let mediaQueryObj = window.matchMedia('(prefers-color-scheme: dark)');
 let isDarkMode = mediaQueryObj.matches;
 
 if (navigator.userAgent.match(/samsung/i)) {
-  if (isDarkMode) {
-    alert("Your browser (Samsung Internet) may not show this website" + 
-    "correctly. pLease consider using a standards-compliant" + 
+  if (true) {
+    alert("Your browser (Samsung Internet) may not show this website " + 
+    "correctly. Please consider using a standards-compliant " + 
     "browser instead. \n\n" + 
     "We recommend FireFox, Microsoft Edge, or Google Chrome.");
   }
-}
+}*/
