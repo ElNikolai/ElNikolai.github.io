2c800193e9ea922369746ea815475cac807c9b58
marc.prem09@gmail.com <marc.prem09@gmail.com>
2023-12-22 12:07:02 +0100

    pageTransition

--- OwnWebsite/script.js
+++ OwnWebsite/script.js
@@ -9,6 +9,38 @@ if (navigator.userAgent.match(/samsung/i)) {
     "We recommend FireFox, Microsoft Edge, or Google Chrome.");
   }
 }*/
+
+//-------------------page transition-------------------------
+const transition_el = document.querySelector('.transition');
+
+window.onload = () => {
+  const anchors = document.querySelectorAll('.link');
+  
+  setTimeout(() => {
+    transition_el.classList.remove('is-active');
+  }, 500);
+  
+  for (let i = 0; i < anchors.length; i++) {
+    const anchor = anchors[i];
+    
+    anchor.addEventListener('click', e => {
+      e.preventDefault();
+      let target = e.target.href;
+      loadPage(target);
+    });
+  }
+}
+
+function loadPage(_target) {
+  transition_el.classList.add('is-active');
+  
+  setTimeout(() => {
+    window.location.href = _target;
+  }, 500);
+}
+
+
+//----------------elements transition---------------------
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     console.log(entry);
